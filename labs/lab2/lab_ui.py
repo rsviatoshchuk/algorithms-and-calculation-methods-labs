9a26eab9fa5e54004212381452e54828a3a2cbd7
Rostyslav Sviatoshchuk <svyatoschuk@gmail.com>
2020-03-01 19:47:07 +0200

    basic ui for Merge Sort added

--- labs/lab2/lab_ui.py
+++ labs/lab2/lab_ui.py
@@ -8,14 +8,21 @@ from os import remove
 class LabWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.init_window()
-        self.center()
 
-    def init_window(self):
         self.setWindowTitle("Lab2")
         self.setMinimumSize(QSize(600, 500))
         self.setStyleSheet("background-color:#2D3047; color:black; font-family: 'Open Sans Condensed'; font-size: 24px")
 
+        tabs = QTabWidget()
+        tabs.addTab(MergeSortUI(), "Sort")
+        tabs.addTab(SortTest(), "TimeTest")
+        tabs.setStyleSheet("background-color:#767BA3")
+
+        vb = QVBoxLayout()
+        vb.addWidget(tabs)
+        self.setLayout(vb)
+
+        self.center()
 
     def center(self):
         """function, that center window on the screen"""
@@ -28,6 +35,31 @@ class LabWindow(QWidget):
 class MergeSortUI(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.vb = QVBoxLayout()
+
+        self.load_file_button = QPushButton("Load file")
+        self.load_file_button.clicked.connect(self.load_file)
+        self.vb.addWidget(self.load_file_button)
+
+        self.sorting_array_entry_field = QLineEdit()
+        self.vb.addWidget(self.sorting_array_entry_field)
+
+        self.sort_button = QPushButton("Sort")
+        self.sort_button.clicked.connect(self.sort)
+        self.vb.addWidget(self.sort_button)
+
+        self.sorted_array_entry_field = QLineEdit()
+        self.vb.addWidget(self.sorted_array_entry_field)
+
+        self.save_to_file_button = QPushButton("Save to file")
+        self.save_to_file_button.clicked.connect(self.save_to_file)
+        self.save_to_file_button.hide()
+        self.vb.addWidget(self.save_to_file_button)
+
+        self.vb.addStretch(1)
+
+        self.setLayout(self.vb)
 
     def load_file(self):
         try:
@@ -52,6 +84,9 @@ class MergeSortUI(QWidget):
                                         "Successfully saved in {}".format(file_name + str(counter) + ".txt"),
                                         QMessageBox.Ok)
                 break
+
+    def sort(self):
+        pass
 
 
 class SortTest(QWidget):
